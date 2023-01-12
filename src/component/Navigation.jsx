c250306ff12553c14de08c436c521e67a5252649
farrel216 <farrel.arp@gmail.com>
2023-01-12 20:54:32 +0700

    logout handler

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -1,8 +1,25 @@
 import { Navbar, Dropdown, Avatar, Button } from "flowbite-react"
+import { useAuth } from '../context/AuthContext'
 import profile from '../assets/placeholder/profile-photo.jpeg'
+import { useNavigate } from "react-router"
 
-const Navigation = ({isLogin}) => {
+const Navigation = ({ isLogin }) => {
+    const auth = useAuth()
+    const navigate = useNavigate()
     const pathname = window.location.pathname
+
+    const logoutHandler = async () => {
+        try {
+            await auth.logout()
+            navigate('/login')
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
     return (
         <Navbar
             fluid={true}
@@ -41,33 +58,33 @@ const Navigation = ({isLogin}) => {
                         </Dropdown.Item>
                         <Dropdown.Divider />
                         <Dropdown.Item>
-                            <a href="/login">Sign Out</a>
+                            <button onClick={() => { logoutHandler() }}>Sign Out</button>
                         </Dropdown.Item>
                     </Dropdown>
-                        ):(<div className="hidden md:flex ">
-                        <div className="grid grid-cols-2 gap-2">
-                            <button className="text-green-700 border border-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Login</button>
-                            <Button color="success">
-                                Register
-                            </Button>
-                        </div></div>
-                        )}
+                ) : (<div className="hidden md:flex ">
+                    <div className="grid grid-cols-2 gap-2">
+                        <button className="text-green-700 border border-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Login</button>
+                        <Button color="success">
+                            Register
+                        </Button>
+                    </div></div>
+                )}
             </div>
             <Navbar.Toggle />
             <Navbar.Collapse>
                 <Navbar.Link active={pathname === "/home"}
-                    href="/home" 
+                    href="/home"
                 >
                     Home
                 </Navbar.Link>
-                <Navbar.Link 
-                active={pathname === "/category/jewelery"}
-                href="/category/jewelery">
+                <Navbar.Link
+                    active={pathname === "/category/jewelery"}
+                    href="/category/jewelery">
                     Jewelery
                 </Navbar.Link>
-                <Navbar.Link 
-                active={pathname === "/category/electronics"}
-                href="/category/electronics">
+                <Navbar.Link
+                    active={pathname === "/category/electronics"}
+                    href="/category/electronics">
                     Electronics
                 </Navbar.Link>
                 <Navbar.Link href="/navbars">
@@ -76,10 +93,10 @@ const Navigation = ({isLogin}) => {
                 <Navbar.Link href="/navbars">
                     Contact
                 </Navbar.Link>
-                <Navbar.Link className={isLogin?('hidden'):("md:hidden") } href="/navbars">
-                    Login   
+                <Navbar.Link className={isLogin ? ('hidden') : ("md:hidden")} href="/navbars">
+                    Login
                 </Navbar.Link>
-                <Navbar.Link className={isLogin?('hidden'):("md:hidden")} href="/navbars">
+                <Navbar.Link className={isLogin ? ('hidden') : ("md:hidden")} href="/navbars">
                     Register
                 </Navbar.Link>
             </Navbar.Collapse>
