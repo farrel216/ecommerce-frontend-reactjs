6a2eeddfce694d27776f08ee7ce8c4e6c8c419be
farrel216 <farrel.arp@gmail.com>
2023-01-12 21:39:18 +0700

    mongodb atlas

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -63,8 +63,8 @@ const Navigation = ({ isLogin }) => {
                     </Dropdown>
                 ) : (<div className="hidden md:flex ">
                     <div className="grid grid-cols-2 gap-2">
-                        <button className="text-green-700 border border-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Login</button>
-                        <Button color="success">
+                        <a href="/login" className="btn text-green-700 border border-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Login</a>
+                        <Button onClick={navigate('/register')} color="success">
                             Register
                         </Button>
                     </div></div>
@@ -87,16 +87,10 @@ const Navigation = ({ isLogin }) => {
                     href="/category/electronics">
                     Electronics
                 </Navbar.Link>
-                <Navbar.Link href="/navbars">
-                    Pricing
-                </Navbar.Link>
-                <Navbar.Link href="/navbars">
-                    Contact
-                </Navbar.Link>
-                <Navbar.Link className={isLogin ? ('hidden') : ("md:hidden")} href="/navbars">
+                <Navbar.Link className={isLogin ? ('hidden') : ("md:hidden")} href="/login">
                     Login
                 </Navbar.Link>
-                <Navbar.Link className={isLogin ? ('hidden') : ("md:hidden")} href="/navbars">
+                <Navbar.Link className={isLogin ? ('hidden') : ("md:hidden")} href="/register">
                     Register
                 </Navbar.Link>
             </Navbar.Collapse>
