526a0dea101bea54e4c7c5afaedcafaefe0be5d0
farrel216 <farrel.arp@gmail.com>
2023-01-21 17:23:45 +0700

    cart provider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
-import {CartProvider} from "react-use-cart"
+import {CartProvider} from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
